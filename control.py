62dd45f4a137ed1e8fa9a5ffac24ef71fa0e256a
PaulMares <fpmaresquiersanchez@gmail.com>
2024-12-12 15:11:11 -0800

    Added drag for non-OS X platforms

--- control.py
+++ control.py
@@ -6,8 +6,9 @@ from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 import numpy as np
 import cv2
-import pyautogui
+import pyautogui as pag
 from math import sqrt
+import platform
 
 
 def draw_landmarks_on_image(rgb_image, detection_result):
@@ -48,8 +49,9 @@ def is_pinch(detection_result):
 	return False
 
 def get_result(result, output_image, timestamp_ms):
-	global img, landmarks
+	global img, landmarks, prev_time
 	img = output_image.numpy_view()
+	prev_time = timestamp_ms
 	if len(result.hand_landmarks) > 0:
 		landmarks = result
 	else:
@@ -64,17 +66,23 @@ VisionRunningMode = vision.RunningMode
 cam = cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FPS, 60)
 
-SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
-CLICK_THRESHOLD = 0.05
+PLATFORM = platform.system()
+
+SCREEN_WIDTH, SCREEN_HEIGHT = pag.size()
+CLICK_THRESHOLD = 0.08
+DRAG_THRESHOLD_SECS = 0.5
+click = 0
 
 # Create a hand landmarker instance with the image mode:
 options = HandLandmarkerOptions(
-	base_options=BaseOptions(model_asset_path=r"C:\Users\Paul Maresquier\Documents\Code\Python\HandControl\hand_landmarker.task"),
+	base_options=BaseOptions(model_asset_path=r"/Users/paul/Documents/Python/handControl/hand_landmarker.task",
+	                         delegate=BaseOptions.Delegate.GPU),
 	num_hands=1,
 	running_mode=VisionRunningMode.LIVE_STREAM,
 	result_callback=get_result)
 with HandLandmarker.create_from_options(options) as landmarker:
 	timestamp = time.time()
+	prev_time = timestamp
 	img = cam.read()[1]
 	landmarks = None
 	while True:
@@ -82,15 +90,29 @@ with HandLandmarker.create_from_options(options) as landmarker:
 
 		# Display the captured frame
 		if frame is not None:
-			mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+			mp_image = mp.Image(image_format=mp.ImageFormat.SRGBA, data=cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA))
 			landmarker.detect_async(mp_image, int((time.time() - timestamp) * 1000))
 
 		if landmarks is not None:
-			land_img = draw_landmarks_on_image(img, landmarks)
+			land_img = draw_landmarks_on_image(cv2.cvtColor(img, cv2.COLOR_RGBA2RGB), landmarks)
 			hand = get_average_hand_location(landmarks)
-			if is_pinch(landmarks):
-				pyautogui.leftClick()
-			pyautogui.moveTo(hand[0], hand[1])
+			pinch = is_pinch(landmarks)
+			if pinch and click == 0:
+				click = time.time()
+			elif pinch and click > 0 and time.time() - click > DRAG_THRESHOLD_SECS:
+				pag.mouseDown()
+				click = -1
+			elif not pinch and click > 0:
+				pag.leftClick()
+				click = 0
+			elif not pinch and click == -1:
+				pag.mouseUp()
+				click = 0
+
+			if click >= 0 or platform not in ["Windows", "Linux"]:
+				pag.moveTo(hand[0], hand[1], _pause=False)
+			else:
+				pag.dragTo(hand[0], hand[1], button="left", _pause=False)
 			cv2.imshow('Camera', land_img)
 		else:
 			cv2.imshow('Camera', img)
@@ -99,6 +121,6 @@ with HandLandmarker.create_from_options(options) as landmarker:
 		if cv2.waitKey(1) == ord('q'):
 			break
 
-	# Release the capture and writer objects
+	# Release the capture object
 	cam.release()
 	cv2.destroyAllWindows()
